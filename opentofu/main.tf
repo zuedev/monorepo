ff842a27d84cd46fb9f83a3eca968f3057874523
Alex <zuedev@gmail.com>
2025-08-27 16:19:08 +0100

    move aws to aws.tf

--- opentofu/main.tf
+++ opentofu/main.tf
@@ -13,19 +13,3 @@ terraform {
     }
   }
 }
-
-provider "aws" {
-  region = "eu-west-2"
-}
-
-resource "aws_s3_bucket" "git-zue-dev-monorepo-opentofu-state" {
-  bucket = "git-zue-dev-monorepo-opentofu-state"
-}
-
-resource "aws_s3_bucket_versioning" "git-zue-dev-monorepo-opentofu-state" {
-  bucket = aws_s3_bucket.git-zue-dev-monorepo-opentofu-state.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
