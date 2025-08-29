7b3400c0a6d9b0addca24e7ef452c7acf162597a
Alex Pooley (@zuedev) <zuedev@gmail.com>
2025-08-29 02:03:38 +0100

    add example environment files for bash and PowerShell, and update .gitignore

---
+++ opentofu/.env.example.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# This is an example environment file specifically designed for a working bash environment.
+# Copy this file to .env (which is gitignored) and fill in real values, then source it.
+# Example: source .env
+
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY=""
+export GOOGLE_CREDENTIALS=""
