91d236a75b7ee4b0538104ee843bc4a42f7b5035
Alex <zuedev@gmail.com>
2025-08-30 16:20:59 +0100

    fix ps1 instructions

--- opentofu/.env.example.ps1
+++ opentofu/.env.example.ps1
@@ -1,5 +1,5 @@
 # This is an example environment file specifically designed for a working PowerShell environment.
-# Copy this file to .env (which should be gitignored) and fill in real values, then dot-source it.
+# Copy this file to .env.ps1 (which should be gitignored) and fill in real values, then dot-source it.
 # Usage: . .\.env.ps1
 
 $env:AWS_ACCESS_KEY_ID = ""
