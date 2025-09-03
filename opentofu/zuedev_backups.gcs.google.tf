8111406ade1cf5a473a73b20d2f0c6c2501c853f
Alex Pooley (@zuedev) <zuedev@gmail.com>
2025-09-03 14:56:00 +0100

    remove all buckets (reset!)

--- opentofu/zuedev_backups.gcs.google.tf
+++
@@ -1,7 +0,0 @@
-resource "google_storage_bucket" "zuedev_backups" {
-  name          = "zuedev-backups"
-  location      = "EUROPE-WEST2"
-  force_destroy = true
-
-  uniform_bucket_level_access = true
-}
--- opentofu/zuedev_git_repos.s3.aws.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_s3_bucket" "zuedev_git_repos" {
-  bucket = "zuedev-git-repos"
-}
