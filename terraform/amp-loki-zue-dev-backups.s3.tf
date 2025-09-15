75799fe7be4a30e8f5f00969f2f4a045c388dcff
Alex Pooley (@zuedev) <zuedev@gmail.com>
2025-09-15 14:23:07 +0100

    add IAM user and policy for S3 bucket access in amp_loki_zue_dev_backups

--- terraform/amp-loki-zue-dev-backups.s3.tf
+++ terraform/amp-loki-zue-dev-backups.s3.tf
@@ -1,3 +1,35 @@
 resource "aws_s3_bucket" "amp_loki_zue_dev_backups" {
   bucket = "amp-loki-zue-dev-backups"
 }
+
+resource "aws_iam_user" "amp_loki_zue_dev_backups_user" {
+  name = "amp-loki-zue-dev-backups-user"
+}
+
+resource "aws_iam_user_policy" "amp_loki_zue_dev_backups_user_policy" {
+  name = "amp-loki-zue-dev-backups-user-policy"
+  user = aws_iam_user.amp_loki_zue_dev_backups_user.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket",
+          "s3:GetBucketLocation"
+        ]
+        Resource = aws_s3_bucket.amp_loki_zue_dev_backups.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:DeleteObject"
+        ]
+        Resource = "${aws_s3_bucket.amp_loki_zue_dev_backups.arn}/*"
+      }
+    ]
+  })
+}
