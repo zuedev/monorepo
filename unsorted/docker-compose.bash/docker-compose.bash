688e19fe36351fe95a029f1715013b1924829877
Alex Pooley (@zuedev) <zuedev@gmail.com>
2025-12-24 18:26:04 +0000

    Refactor docker-compose script to use a function for checking compose files, improving maintainability and readability.

--- unsorted/docker-compose.bash/docker-compose.bash
+++ unsorted/docker-compose.bash/docker-compose.bash
@@ -5,6 +5,18 @@ ROOT_DIR="/mnt/user/root/docker-compose"
 # make sure we're in the right directory
 cd $ROOT_DIR
 
+# list of compose files to detect (in order of priority)
+COMPOSE_FILES=("compose.yaml" "compose.yml" "docker-compose.yaml" "docker-compose.yml")
+
+function has_compose_file {
+    for file in "${COMPOSE_FILES[@]}"; do
+        if [ -f "$1/$file" ]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
 function execute_in_subdir {
     # check if directory exists
     if [ -d "$1" ]; then
@@ -18,8 +30,8 @@ function execute_in_all_subdirs {
     for dir in $(ls -d */); do
         # remove trailing slash
         dir=${dir%?}
-        # check if directory contains compose.yaml
-        if [ -f "$dir/compose.yaml" ]; then
+        # check if directory contains a compose file
+        if has_compose_file "$dir"; then
             # run docker compose
             cd $dir && $@ && cd ..
         fi
@@ -27,16 +39,16 @@ function execute_in_all_subdirs {
 }
 
 function dc_pull_up {
-    # check if directory contains compose.yaml
-    if [ -f "$1/compose.yaml" ]; then
+    # check if directory contains a compose file
+    if has_compose_file "$1"; then
         # run docker compose
         cd $ROOT_DIR/$1 && docker compose pull && docker compose up -d --remove-orphans && cd $ROOT_DIR
     fi
 }
 
 function dc_build_up {
-    # check if directory contains compose.yaml
-    if [ -f "$1/compose.yaml" ]; then
+    # check if directory contains a compose file
+    if has_compose_file "$1"; then
         # run docker compose
         cd $ROOT_DIR/$1 && docker compose build && docker compose up -d --remove-orphans && cd $ROOT_DIR
     fi
